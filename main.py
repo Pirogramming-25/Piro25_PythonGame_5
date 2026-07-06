3ca2b320d5c1432cf039375e1fd2149e9ef4c0aa
daynight0219-ux <yewongrey@naver.com>
2026-07-06 11:46:41 +0900

    fix:main의 게임리스트에서 예원 게임 이름 수정

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ COMPUTER_CANDIDATES = [
 GAME_LIST = [
     ("지수의 게임", jisoo_game.play_game),
     ("연우의 소수파 술자리 밸런스 게임", yeonwoo_game.play_game),
-    ("예원의 게임", yewon_game.play_game),
+    ("예원의 취중 베스킨라빈스 31게임", yewon_game.play_game),
 ]
 
 
